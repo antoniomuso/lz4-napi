15e01751821a21aae067e4261a2b61ebe60ca224
antoniomuso <antoniomusolino007@gmail.com>
2021-12-04 10:52:08 +0100

    test: add tests on types and errors

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -25,3 +25,30 @@ test('compress decompress sync should work', (t) => {
   const decompressed = uncompressSync(compressed)
   t.is(before.toString('utf8'), decompressed.toString('utf8'))
 })
+
+test('should throw a promise error if data is not compressed', async (t) => {
+  await t.throwsAsync(uncompress(stringToCompress))
+})
+
+test('should throw a TypeError if data is not a String | ArrayBuffer | Buffer | Uint8Array', (t) => {
+  t.throws(() => compress({ hello: 'msts' } as unknown as string))
+})
+
+test('compress should take all input types', async (t) => {
+  const outArr = []
+  outArr.push(await compress(stringToCompress))
+  outArr.push(await compress(Buffer.from(stringToCompress)))
+  outArr.push(await compress(new Uint8Array(Buffer.from(stringToCompress))))
+
+  t.is(new Set(outArr.map((buff) => buff.toString())).size, 1)
+})
+
+test('uncompress should take all input types', async (t) => {
+  const compressedValue = compressSync(stringToCompress)
+  const outArr = []
+  outArr.push(await uncompress(compressedValue))
+  outArr.push(await uncompress(Buffer.from(compressedValue)))
+  outArr.push(await uncompress(new Uint8Array(Buffer.from(compressedValue))))
+
+  t.is(new Set(outArr.map((buff) => buff.toString())).size, 1)
+})
