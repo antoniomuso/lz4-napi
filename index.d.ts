999cc052aca3e4a05add2aad2b0b31e8acd4380e
Mark Erikson <mark@isquaredsoftware.com>
2025-08-28 16:38:25 -0400

    Add Encoder/DecoderStream implementation

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,17 @@
 // Re-export all native bindings
 export * from './nativeBindings'
+
+// Import Transform stream types
+import { Transform, TransformOptions } from 'stream'
+
+// Node.js Transform streams (high-level API)
+export declare class EncoderStream extends Transform {
+  constructor(options?: TransformOptions)
+}
+
+export declare class DecoderStream extends Transform {
+  constructor(options?: TransformOptions)
+}
+
+export declare function createEncoderStream(options?: TransformOptions): EncoderStream
+export declare function createDecoderStream(options?: TransformOptions): DecoderStream
--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // Re-export all native bindings
 module.exports = require('./nativeBindings')
+
+// Import and add Transform stream wrappers
+const { EncoderStream, DecoderStream, createEncoderStream, createDecoderStream } = require('./lib')
+
+// Export Node.js Transform streams (high-level API)
+module.exports.EncoderStream = EncoderStream
+module.exports.DecoderStream = DecoderStream
+module.exports.createEncoderStream = createEncoderStream
+module.exports.createDecoderStream = createDecoderStream
